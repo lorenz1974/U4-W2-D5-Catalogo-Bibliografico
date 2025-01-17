5b225a37142979ecc2ef0147ca17cfc90817916f
Lorenz <l.lione@remoteitalia.com>
2025-01-17 16:45:14 +0100

    First commit.
    Gestione salvataggio dati e lettura su file JSON.
    Presentazione dati da toString in formato tabellare e simil JSON.
    AnySearch per la ricerca in tutti i singoli campi.

---
+++ src/main/java/Lorenz/Application.java
@@ -0,0 +1,8 @@
+package Lorenz;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/Lorenz/Libro.java
@@ -0,0 +1,4 @@
+package Lorenz;
+
+public class Libro {
+}
